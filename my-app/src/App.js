1619bf2d802d89959fb7f7401e6cf2f2e5c51328
Loopypew <fivebailey2@gmail.com>
2022-06-27 23:23:54 +1200

    Initial react set up

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Test Website</h1>
+    <a href=""> <p>Click here to move to the next page</p> </a>
+    </div>
+  );
+}
+
+export default App;
